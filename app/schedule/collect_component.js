e7455f02037b79a592e7478544694795c30b1133
“kangyu” <kangyu@huatu.com>
2023-11-21 11:10:34 +0800

    feat: 采集数据完成

--- app/schedule/collect_component.js
+++ app/schedule/collect_component.js
@@ -1,4 +1,4 @@
-import { componentStatic } from './../utils/statistics';
+const ComponentStatic = require('./../util/statistics.js').ComponentStatic;
 const Subscription = require('egg').Subscription;
 const Sites = {
   // 'mobile-screen': 'https://mobile-screen.huatu.com',
@@ -12,16 +12,13 @@ class UpdateCache extends Subscription {
   static get schedule() {
     return {
       // 每日3点准点执行一次：'0 0 3 ? * *'
-      cron: '0 47 11 ? * *',
+      cron: '0 10 11 ? * *',
       type: 'all', // 指定所有的 worker 都需要执行
     };
   }
 
   // subscribe 是真正定时任务执行时被运行的函数
   async subscribe() {
-    // const res = await this.ctx.curl(`${Sites['beta-mobile-screen']}/count.js?time=${timestamp}`, {
-    //   dataType: 'json',
-    // });
     const { data } = await this.ctx.curl('http://localhost:7001/site/list', {
       dataType: 'json',
     });
@@ -31,14 +28,8 @@ class UpdateCache extends Subscription {
       const res = await this.ctx.curl(`${site.site}/count.js?time=${timestamp}`, {
         dataType: 'json',
       });
-
-      const staticDate = await componentStatic(site, res);
-      const result = await this.ctx.app.mysql.insert('components_coverage', { ...staticDate }); // 在 post 表中，插入 title 为 Hello World 的记录
-
-      // let static = componentStatic(site,res)
-      // const result = await this.ctx.app.mysql.insert('components_coverage', { date: timestamp, project: site.name, components_count: res.data.components_count, components_coverage_count: res.data.components_coverage_count, components_times: res.data.components_times, pages_count: res.data.pages_count, pages_coverage_count: res.data.pages_coverage_count, pages_times: res.data.pages_times }); // 在 post 表中，插入 title 为 Hello World 的记录
-
-      console.log(result);
+      const staticDate = await ComponentStatic(site, res.data);
+      const result = await this.ctx.app.mysql.insert('components_coverage',staticDate);
     }
   }
 }
--- app/util/statistics.js
+++ app/util/statistics.js
@@ -1,67 +1,28 @@
 
-// {
-//     component1:{
-//         components: [],
-//         pages: [],
-//         coverage: {
-//             components: '100%',
-//             pages: '100%'
-//         }
-//     },
-//     }
-// }
-export const mergeObj = (obj1, obj2) => {
-  Object.keys(obj2).forEach(key => {
-    if (obj1[key]) {
-      obj1[key].components = obj1[key].components.concat(obj2[key].components);
-      obj1[key].pages = obj1[key].pages.concat(obj2[key].pages);
-    } else {
-      obj1[key] = obj2[key];
-    }
-  });
-  return obj1;
-};
 
-// 通过索引把2个数组合并为对象
-// {
-//     app1: {
-//         components: [],
-//         pages: [],
-//         coverage: {
-//             components: '100%',
-//             pages: '100%'
-//         }
-//     },
-//   }
-// }
-export const mergeArr = (arr1, arr2) => {
-  const obj = {};
-  arr1.forEach((item, index) => {
-    obj[item] = arr2[index];
-  });
-  return obj;
-};
-
-
-export const componentStatic = (site, data) => {
+ const ComponentStatic = (site, data) => {
   const components = Object.keys(data).splice(0, Object.keys(data).length - 2);
   const timestamp = Date.now();
   const staticDate = components.map(item => {
-    const pages = data[item].pages;
-    const pagesCount = pages.length;
-    const pagesCoverageCount = pages.filter(item => item.coverage === '100%').length;
-    const pagesTimes = pages.map(item => item.time);
+    const cmp = data[item];
+    const coverage = cmp.coverage;
+    const pagesTimes = cmp.pages.reduce((total, item) => total + item.pos.length, 0);
+    const componentsTimes = cmp.components.reduce((total, item) => total + item.pos.length, 0);
     return {
-      date: timestamp,
-      component: item,
-      project: site.site,
-      components_count: data[item].components_count,
-      components_coverage_count: data[item].components_coverage_count,
-      components_times: data[item].components_times,
-      pages_count: pagesCount,
-      pages_coverage_count: pagesCoverageCount,
+      date: String(timestamp),
+      component: String(item),
+      project: String(site.site),
+      components_coverage_count: String(coverage.components),
+      pages_coverage_count:  String(coverage.pages),
+      components_count: data.componentsCount,
+      pages_count: data.pagesCount,
+      components_times: componentsTimes,
       pages_times: pagesTimes,
     };
   });
   return staticDate;
 };
+
+module.exports = {
+  ComponentStatic
+}
