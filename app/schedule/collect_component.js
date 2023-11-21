86f8b32b591bcd222160d4ff9791de571db1fc8f
“kangyu” <kangyu@huatu.com>
2023-11-21 11:36:10 +0800

    feat: data collect

--- app/schedule/collect_component.js
+++ app/schedule/collect_component.js
@@ -1,18 +1,11 @@
 const ComponentStatic = require('./../util/statistics.js').ComponentStatic;
 const Subscription = require('egg').Subscription;
-const Sites = {
-  // 'mobile-screen': 'https://mobile-screen.huatu.com',
-  'beta-mobile-screen': 'https://beta-mobile-screen.huatu.com',
-  // 'beta-screen-manager': 'https://beta-screen-manager.huatu.com'
-  // 'test-screen-manager': 'https://test-screen-manager.huatu.com'
-};
-
 class UpdateCache extends Subscription {
   // 通过 schedule 属性来设置定时任务的执行间隔等配置
   static get schedule() {
     return {
       // 每日3点准点执行一次：'0 0 3 ? * *'
-      cron: '0 10 11 ? * *',
+      cron: '0 35 11 ? * *',
       type: 'all', // 指定所有的 worker 都需要执行
     };
   }
@@ -30,6 +23,7 @@ class UpdateCache extends Subscription {
       });
       const staticDate = await ComponentStatic(site, res.data);
       const result = await this.ctx.app.mysql.insert('components_coverage',staticDate);
+      console.log(result);
     }
   }
 }
