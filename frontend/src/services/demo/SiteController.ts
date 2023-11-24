7254d8c69ccb6e51b7eb2cdec6ed6e652ee0d4c3
“kangyu” <kangyu@huatu.com>
2023-11-24 08:56:58 +0800

    feat: interface

---
+++ frontend/src/services/demo/SiteController.ts
@@ -0,0 +1,99 @@
+/* eslint-disable */
+// 该文件由 OneAPI 自动生成，请勿手动修改！
+import { request } from '@umijs/max';
+
+/** 此处后端没有提供注释 GET /api/v1/queryUserList */
+export async function querySiteList(
+  params: {
+    // query
+    // /** keyword */
+    // keyword?: string;
+    // /** current */
+    // current?: number;
+    // /** pageSize */
+    // pageSize?: number;
+  },
+  options?: { [key: string]: any },
+) {
+  return request<API.Result_PageInfo_SiteInfo__>('/api/site/list', {
+    method: 'GET',
+    params: {
+      ...params,
+    },
+    ...(options || {}),
+  });
+}
+
+/** 此处后端没有提供注释 POST /api/v1/user */
+export async function addSite(
+  params?: any,
+  body?: API.SiteInfo,
+  options?: { [key: string]: any },
+) {
+  return request<API.Result_UserInfo_>('/api/site/add', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    params: {
+      ...params,
+    },
+    data: body,
+    ...(options || {}),
+  });
+}
+
+/** 此处后端没有提供注释 GET /api/v1/user/${param0} */
+export async function getUserDetail(
+  params: {
+    // path
+    /** userId */
+    userId?: string;
+  },
+  options?: { [key: string]: any },
+) {
+  const { userId: param0 } = params;
+  return request<API.Result_UserInfo_>(`/api/v1/user/${param0}`, {
+    method: 'GET',
+    params: { ...params },
+    ...(options || {}),
+  });
+}
+
+/** 此处后端没有提供注释 PUT /api/v1/user/${param0} */
+export async function modifyUser(
+  params: {
+    // path
+    /** userId */
+    userId?: string;
+  },
+  body?: API.UserInfoVO,
+  options?: { [key: string]: any },
+) {
+  const { userId: param0 } = params;
+  return request<API.Result_UserInfo_>(`/api/v1/user/${param0}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    params: { ...params },
+    data: body,
+    ...(options || {}),
+  });
+}
+
+/** 此处后端没有提供注释 DELETE /api/v1/user/${param0} */
+export async function deleteSite(
+  params: {
+    // path
+    /** ids */
+    ids?: String | undefined;
+  },
+  options?: { [key: string]: any },
+) {
+  return request<API.Result_string_>(`/api/site/del`, {
+    method: 'DELETE',
+    params: { ...params },
+    ...(options || {}),
+  });
+}
