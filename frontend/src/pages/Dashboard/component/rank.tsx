0e0a7434b033bbd8e4c40ddc2d655fb048efa357
“kangyu” <kangyu@huatu.com>
2023-11-27 09:48:05 +0800

    feat: format

--- frontend/src/pages/Dashboard/component/rank.tsx
+++ frontend/src/pages/Dashboard/component/rank.tsx
@@ -27,37 +27,38 @@ const App: React.FC = () => {
     setrRankType(e.target.value);
   };
 
-  return (<Card
-            // loading={loading}
-            bordered={false}
-            title='组件Rank Top10'
-            style={{ marginTop: 24 }}
-            extra={
-              <div>
-                  <Radio.Group value={rankType} onChange={(e)=>handleChangerankType(e)}>
-                    <Radio.Button value="1">
-                    页面
-                    </Radio.Button>
-                    <Radio.Button value="2">
-                    组件
-                    </Radio.Button>
-                  </Radio.Group>
-              </div>
-            }
-          >
-      <List
-        itemLayout="horizontal"
-        dataSource={rank}
-        renderItem={(item, index) => (
-          <List.Item>
-            <List.Item.Meta
-              avatar={<Avatar style={{ backgroundColor: rankColor[index], color: '#fff' }}>{item.component.substr(0,1).toUpperCase()}</Avatar>}
-              title={<a href="https://ant.design"><b>Top {index+1}</b> <span style={{'color':'#55da9e'}}>{item.component}</span></a>}
-              description={`${item.project}: 调用${rankType === '1' ? item.pages_times : item.components_times}次,覆盖率${rankType === '1' ? (item.pages_coverage_count/item.pages_count*100).toFixed(2) : (item.components_coverage_count/item.components_count*100).toFixed(2)}%`}
-            />
-          </List.Item>
-        )}
-      />
+  return (
+  <Card
+    // loading={loading}
+    bordered={false}
+    title='组件Rank Top10'
+    style={{ marginTop: 24 }}
+    extra={
+      <div>
+          <Radio.Group value={rankType} onChange={(e)=>handleChangerankType(e)}>
+            <Radio.Button value="1">
+            页面
+            </Radio.Button>
+            <Radio.Button value="2">
+            组件
+            </Radio.Button>
+          </Radio.Group>
+      </div>
+    }
+  >
+    <List
+      itemLayout="horizontal"
+      dataSource={rank}
+      renderItem={(item, index) => (
+        <List.Item>
+          <List.Item.Meta
+            avatar={<Avatar style={{ backgroundColor: rankColor[index], color: '#fff' }}>{item.component.substr(0,1).toUpperCase()}</Avatar>}
+            title={<a href="https://ant.design"><b>Top {index+1}</b> <span style={{'color':'#55da9e'}}>{item.component}</span></a>}
+            description={`${item.project}: 调用${rankType === '1' ? item.pages_times : item.components_times}次,覆盖率${rankType === '1' ? (item.pages_coverage_count/item.pages_count*100).toFixed(2) : (item.components_coverage_count/item.components_count*100).toFixed(2)}%`}
+          />
+        </List.Item>
+      )}
+    />
   </Card>
 )};
 
